41e421a7ac229df03968530e3fee8ef289b1cc45
CMPT 201 User <armin15.nov@gmail.com>
2026-03-26 17:43:42 -0700

    First commit

---
+++ src/client.c
@@ -0,0 +1,36 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 5) {
+    fprintf(stderr, "Usage %s <IP> <port> <# of message> <log file>\n",
+            argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  int port = atoi(argv[2]);
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock < 0) {
+    perror("Socket error");
+    exit(EXIT_FAILURE);
+  }
+
+  struct sockaddr_in server_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  inet_pton(AF_INET, argv[1], &server_addr.sin_addr);
+
+  if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    perror("connect");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Connected to server!\n");
+  close(sock);
+  return 0;
+}
